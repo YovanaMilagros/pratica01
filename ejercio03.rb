bb714662cd456a1ffdde761878f1142ca9da45fb
YovanaMilagros <yovanahuamanquispe@gmail.com>
2016-04-12 01:13:05 -0500

    proyectos

---
+++ ejercio03.rb
@@ -0,0 +1,11 @@
+ropas=["polo","pantalon","casaca","falda"]
+
+puts " las ropas que utilizo son: " + ropas[2]
+
+puts "========================"
+
+ropas[0..3].each { |n| 
+    puts "mis ropas favoritas son: " +n
+ }
+
+gets()
